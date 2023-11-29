41f3f4160bec68bd79ec1cc451abe4b51155dbcf
sanketss0864 <sanketss0864@gmail.com>
2023-11-29 16:00:56 +0530

    footer and carousel section added

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        center: true,
+        items:1,
+    })
+  });
