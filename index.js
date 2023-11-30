859fd3221afa7571be0efebdd1f6937a73e94ee7
sanketss0864 <sanketss0864@gmail.com>
2023-11-30 13:08:31 +0530

    non responsive desktop layout without navbar

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('#owl-carousel2').owlCarousel({
         loop:true,
         margin:10,
         nav:false,
         center: true,
         items:1,
+    })
+    $('#owl-carousel1').owlCarousel({
+        loop:true,
+        nav:false,
+        items:3,
+        margin:5,
+        dots:true,
     })
   });
